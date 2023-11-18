6873b58a5f116df485990649c851f251fdae78ec
volirra <127318805+volirra@users.noreply.github.com>
2023-11-18 16:10:48 +0200

    commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,63 +5,64 @@ import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Notification from 'components/Notification/Notification';
 import s from 'components/App/App.module.css';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrement = options => {
+    switch (options) {
+      case 'good':
+        setGood(good => good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral => neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad => bad + 1);
+        break;
+
+      default:
+        return;
+    }
   };
 
-  handleIncrement = option => {
-    this.setState(prevState => {
-      return { [option]: prevState[option] + 1 };
-    });
-  };
+  const countTotalFeedback = () =>
+    [good, neutral, bad].reduce((acc, value) => acc + value);
 
-  countTotalFeedback() {
-    const totalValue = Object.values(this.state);
-    return totalValue.reduce((acc, value) => acc + value);
-  }
-
-  countPositiveFeedbackPercentage() {
-    const value = this.countTotalFeedback();
-    const percentage = Math.round((this.state.good / value) * 100);
+  function countPositiveFeedbackPercentage() {
+    const percentage = Math.round((good / value) * 100);
 
     if (!percentage) return 0;
     return percentage;
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const stateName = Object.keys(this.state);
-    const value = this.countTotalFeedback();
-    const percent = this.countPositiveFeedbackPercentage();
+  const optionName = ['good', 'neutral', 'bad'];
+  const value = countTotalFeedback();
+  const percent = countPositiveFeedbackPercentage();
 
-    console.log(value);
+  return (
+    <div className={s.wrapper}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={optionName}
+          onLeaveFeedback={handleIncrement}
+        />
+      </Section>
 
-    return (
-      <div className={s.wrapper}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={stateName}
-            onLeaveFeedback={this.handleIncrement}
+      <Section title="Statistics">
+        {value === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={value}
+            positivePercentage={percent}
           />
-        </Section>
-
-        <Section title="Statistics">
-          {value === 0 ? (
-            <Notification message="No feedback given" />
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={value}
-              positivePercentage={percent}
-            />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        )}
+      </Section>
+    </div>
+  );
 }
